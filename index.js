b5dff1e9ab508780b280c89357f661ff5ee158c1
Ivan Orlovac <ivanorlovac@gmail.com>
2023-11-05 15:00:25 +0100

    Initiera grundläggande applikationsstruktur för TicTacToe

---
+++ index.js
@@ -0,0 +1,8 @@
+const readline = require('readline');
+const TicTacToe = require('./TicTacToe');
+const ScoreManager = require('./ScoreManager');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
